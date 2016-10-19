b1bdf058337a64c40fa5b276eb65913792aa58b0
Rohan Julka <rohan.julka@thepsi.com>
2016-10-19 14:21:30 +0530

    Changes to onesignal script

--- service-worker_new.js
+++ service-worker_new.js
@@ -14,15 +14,37 @@ self.addEventListener('push', function(event) {
   event.waitUntil(self.registration.showNotification(obj.title, {
     body: obj.text,
     tag: obj.title,
-    icon: 'http://beintent.developmentoverview.com/images/browser_notifications_logo.png',
-    badge: 'http://beintent.developmentoverview.com/images/browser_notifications_logo.png',
+    icon: 'https://beintent.developmentoverview.com/images/browser_notifications_logo.png',
+    badge: 'https://beintent.developmentoverview.com/images/browser_notifications_logo.png',
   }));
 });
 
-self.addEventListener('notificationclick', function(event) {
-		console.log(redirect_url);
-		return clients.openWindow(redirect_url);
-	});
+self.addEventListener("notificationclick", function(event) {
+    
+    // close the notification
+    event.notification.close();
+
+    //To open the app after click notification
+    event.waitUntil(
+        clients.matchAll({
+            type: "window"
+        })
+        .then(function(clientList) {
+            for (var i = 0; i < clientList.length; i++) {
+                var client = clientList[i];
+                if ("focus" in client) {
+                    return client.focus();
+                }
+            }
+
+            if (clientList.length === 0) {
+                if (clients.openWindow) {
+                    return clients.openWindow(redirect_url);
+                }
+            }
+        })
+    );
+});
 
 self.onmessage = function(e) {
   port = e.ports[0];
